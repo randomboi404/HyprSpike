71769d5b0661fe53d65221e4feaa6f720b6c3243
randomboi404 <tanishsawant603@gmail.com>
2025-05-08 18:02:07 +0530

    Upload Dotfiles

---
+++ .local/bin/theme-switcher.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <path_to_img>"
+  exit 1
+fi
+
+IMAGE="$1"
+
+notify-send "Changing Theme" "Applying new theme..."
+
+swww img "$IMAGE" --transition-type="fade" --transition-step=1 --transition-fps="60"
+
+wal -i "$IMAGE" -n -s -t -e
